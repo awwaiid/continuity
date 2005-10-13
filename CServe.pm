6b0c81f89165571a85cfb8a91c9e47b1546f97b3
awwaiid <awwaiid@thelackthereof.org>
2005-10-13 04:15:58 -0400

    Add static serving, and serve .pl files as apps
    darcs-hash:20051013081558-4fc8e-ff86895711cd1c84ef54487af8673fdd33d8f35d.gz

--- CServe.pm
+++ CServe.pm
@@ -10,11 +10,13 @@ use Coro::Cont;
 use HTTP::Daemon;
 use HTTP::Status;
 
-use vars qw( %httpConfig );
+use vars qw( %httpConfig $docroot );
 
 %httpConfig = (
-  LocalPort => 8081,
+#  LocalPort => 11111, # 8081,
 );
+
+$docroot = '/home/awwaiid/projects/perl/cserver/docs';
 
 # Take a sub ref and give back a continuation. Just a shortcut
 sub mkcont {
@@ -26,7 +28,7 @@ sub mkcont {
 my $sessionIdCounter;
 sub getSession {
   my ($request) = @_;
-  print "Headers: " . $request->as_string();
+  #print "Headers: " . $request->as_string();
   my $cookieHeader = $request->header('Cookie');
   print "Cookie: $cookieHeader\n";
   print "sessionIdCounter: $sessionIdCounter\n";
@@ -42,9 +44,12 @@ sub getSessionApp {
   my ($sessionId, $appref) = @_;
   my $app;
   if(exists $session{$sessionId}) {
+    print "Found existing app\n";
     $app = $session{$sessionId};
   } else {
+    print "Creating new continuation\n";
     $app = mkcont($appref);
+    print "Calling for initialization\n";
     $app->(); # Call it once for initialization
   }
   return $app;
@@ -55,33 +60,76 @@ sub setSessionApp {
   $session{$sessionId} = $app;
 }
 
+sub mapPath {
+  my ($path) = @_;
+   # some massaging, also makes it more secure
+   $path =~ s/%([0-9a-fA-F][0-9a-fA-F])/chr hex $1/ge;
+   $path =~ s%//+%/%g;
+   $path =~ s%/\.(?=/|$)%%g;
+   1 while $path =~ s%/[^/]+/\.\.(?=/|$)%%;
+
+   # if($path =~ m%^/?\.\.(?=/|$)%) then bad
+
+  return "$docroot$path";
+}
+
+sub sendStatic {
+  my ($c, $path) = @_;
+  my $file;
+  if(-f $path) {
+    local $\;
+    open($file, $path);
+    $c->send_basic_header();
+    select $c;
+    # For now we'll cheat (badly) and use file
+    my $mimetype = `file -bi $path`;
+    chomp $mimetype;
+    # And for now we'll make a raw exception for .html
+    $mimetype = 'text/html' if $path =~ /\.html$/;
+    print "Content-type: $mimetype\r\n\r\n";
+    print <$file>;
+    select STDOUT;
+    print "Static send '$path', Content-type: $mimetype\n";
+  } else {
+    $c->send_error(RC_NOT_FOUND)
+  }
+}
+
+sub runApp {
+  my ($c, $r, $path) = @_;
+  my $appref = sub { require $path };
+
+  my $sessionId = getSession($r);
+  print "Got session $sessionId\n";
+  my $app = getSessionApp($sessionId, $appref);
+  print "Got app\n";
+  $c->send_basic_header();
+  select $c;
+  print "Set-Cookie: sessionid=$sessionId\r\n";
+  print "Content-type: text/html\r\n\r\n";
+  $app->($r);
+  select STDOUT;
+  setSessionApp($sessionId, $app);
+}
+
 sub serve {
-  my ($appref) = @_;
+#  my ($appref) = @_;
 
   my $d = HTTP::Daemon->new(%httpConfig) || die;
   print "Please contact me at: ", $d->url, "\n";
 
-
   async {
-      print "here1\n";
     #while (my $c = $d->accept('Coro::HTTP::Daemon')) {
     while (my $c = $d->accept()) {
-      print "here2\n";
       #while (my $r = $c->get_request) { # Doesn't work
       if(my $r = $c->get_request) {
-        print "here3\n";
-        if(($r->method eq 'GET' || $r->method eq 'POST')
-          && $r->url->path =~ /^\/app/) {
-          my $code = RC_OK;
-          my $sessionId = getSession($r);
-          my $app = getSessionApp($sessionId, $appref);
-          $c->send_basic_header();
-          select $c;
-          print "Set-Cookie: sessionid=$sessionId\r\n";
-          print "Content-type: text/html\r\n\r\n";
-          $app->($r);
-          select STDOUT;
-          setSessionApp($sessionId, $app);
+        if($r->method eq 'GET' || $r->method eq 'POST') {
+          my $path = mapPath($r->url->path);
+          if($path =~ /\.pl$/) {
+            runApp($c, $r, $path);
+          } else {
+            sendStatic($c, $path);
+          }
         } else {
           $c->send_error(RC_NOT_FOUND)
         }
@@ -91,6 +139,8 @@ sub serve {
     }
   }
 }
+
+serve();
 
 1;
 
