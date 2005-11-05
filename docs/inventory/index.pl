651d4b1dbb6e7bf21eea69c70a13fd814fa9205e
awwaiid <awwaiid@thelackthereof.org>
2005-11-05 03:34:02 -0500

    Doc for inventory table schema
    darcs-hash:20051105083402-4fc8e-340b62c9261103f5db68e6b4af28bd63bbeb8821.gz

--- docs/inventory/index.pl
+++ docs/inventory/index.pl
@@ -14,6 +14,18 @@ use Class::DBI::AutoLoader (
   namespace => 'Database'
 );
 
+=pod
+
+Heres the schema (all ONE table!). You'll want to insert a few rows by hand.
+
+CREATE TABLE `InventoryItem` (
+  `inventoryItem_id` int(11) NOT NULL auto_increment,
+  `serial` varchar(50) default NULL,
+  `name` varchar(100) default NULL,
+  PRIMARY KEY  (`inventoryItem_id`)
+);
+
+=cut
 
 package InventoryItem;
 use base 'Component::HTMLView';
