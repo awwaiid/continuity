91629edf20f127386df6fe73ea29265a7c921c9e
awwaiid <awwaiid@thelackthereof.org>
2005-11-28 02:23:09 -0500

    Changes, including adding C::Server::Simple
    darcs-hash:20051128072309-4fc8e-2cef58d9d5e47a90f71ffdd55f2cefc560536e7e.gz

---
+++ Continuity/Server/Simple.pm
@@ -0,0 +1,44 @@
+
+
+
+package Continuity::Server::Simple;
+
+use strict;
+use Coro::Cont;
+use HTTP::Request::Params;
+use base 'Continuity::Server';
+
+sub execCont {
+
+  my ($self, $cont, $request, $conn) = @_;
+
+  my $prev_select = select $conn; # Should maybe do fancier trick than this
+
+  # This should be override-able
+  print "Content-type: text/html\r\n\r\n";
+
+  $cont->($request);
+  
+  select $prev_select;
+}
+
+sub get_request {
+  my ($self) = @_;
+  yield;
+  my ($request) = @_;
+  return $request;
+}
+
+package HTTP::Request;
+
+# Given an HTTP::Request, return a nice hash of name/params
+# This is really just a thin wrapper around HTTP::Request::Params
+sub params {
+  my ($request) = @_;
+  my $parse = HTTP::Request::Params->new({ req => $request });
+  my $params = $parse->params;
+  return $params;
+}
+
+1;
+
--- counter.pl
+++ counter.pl
@@ -1,8 +1,7 @@
 #!/usr/bin/perl
 
 use strict;
-use Continuity::Server;
-use Continuity::Client::CGI;
+use Continuity::Server::Simple;
 use URI::Escape;
 
 =head1 Summary
@@ -14,12 +13,15 @@ code. We even implement our own 'prompt'...
 
 =cut
 
-my $cserver = Continuity::Server->new(
+my $server = Continuity::Server::Simple->new(
     port => 8080,
-    newContinuationSub => \&main,
+    new_cont_sub => \&main,
+    app_path => '/app',
+    # all other requests go through the static sender by default
     # sdw: mapper => \&foo (or mapper => $ob), etc
-    app_path => '/app', # all other requests go through the static sender by default
 );
+
+$server->loop;
 
 # Ask a question and keep asking until they answer
 sub prompt {
@@ -29,45 +31,33 @@ sub prompt {
     my $uri_option = uri_escape($option);
     print qq{<a href="?option=$uri_option">$option</a><br>};
   }
-  my $params = getParsedInput();
+  my $params = $server->get_request->params;
   my $option = $params->{option};
   return $option || prompt($msg, @ops);
 }
 
 sub main {
-  #my $request = shift; # sdw -- passed implicitly the first time this is called
-  # We get to initialize if we like, and the first call to yield actually accepts the first new request, eh?
-
   # When we are first called we get a chance to initialize stuff
   my $count = 0;
 
-  # After we're done with that we enter a loop
+  # After we're done with that we enter a loop. Forever.
   while(1) {
-    #my $request = $request->next_request;  # sdw -- or something. not sure about a good name. this is where the 'yield' happens.
-    #my $params = $request->params;         # sdw -- or maybe params should be directly accessible through $request
-    my $params = getParsedInput();
+    my $params = $server->get_request->params;
     my $add = $params->{add};
     if($count >= 0 && $count + $add < 0) {
-      my $choice = prompt("Do you really want to GO NEGATIVE?",
-        "Yes", "No");
+      my $choice = prompt("Do you really want to GO NEGATIVE?", "Yes", "No");
       $add = 0 if $choice eq 'No';
     }
     $count += $add;
     print qq{
       Count: $count<br>
-      <a href="?add=1">++</a>
-      &nbsp;&nbsp;
-      <a href="?add=-1">--</a>
+      <a href="?add=1">++</a> &nbsp;&nbsp; <a href="?add=-1">--</a><br>
     };
     if($count == 42) {
-      print "<h1>The Answer To Life, The Universe, and Everything</h1>";
+      print "<h1>The Answer to Life, The Universe, and Everything</h1>";
     }
   }
 }
-
-
-$cserver->loop(); # rbw: an alias to Event::loop :)
-
 
 1;
 
