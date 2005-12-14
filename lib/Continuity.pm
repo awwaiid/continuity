c03af70a9228f7c54f23f4f2e1a92ecf2cd45120
awwaiid <awwaiid@thelackthereof.org>
2005-12-14 00:06:35 -0500

    Move stuff around to fit better in CPAN
    darcs-hash:20051214050635-4fc8e-ddf6bdf540a92ba88125e63ef657478e118ff0ef.gz

---
+++ lib/Continuity.pm
@@ -0,0 +1,48 @@
+package Continuity;
+
+our $VERSION = '0.5';
+
+1;
+__END__
+
+=head1 NAME
+
+Continuity - Simplify web apps through abstraction (such as continuations)
+
+=head1 SYNOPSIS
+
+  use Continuity::Server::Simple;
+  $server = new Continuity::Server::Simple;
+  $server->loop();
+
+  sub main {
+    # must do a substr to chop the leading '/'
+    $name = substr($server->get_request->url->path,1) || 'World';
+    print "Hello, $name!";
+    $name = substr($server->get_request->url->path,1) || 'World';
+    print "Hello to you too, $name!";
+  }
+
+=head1 DESCRIPTION
+
+Continuity seeks to be a library (not a framework) to simplify web
+applications.At the core is a continuation server, which inverts control back
+to the programmer. This is ALPHA software, and feedback/code is welcomed.
+
+=head1 SEE ALSO
+
+Website/Wiki: http://continuity.tlt42.org/
+
+=head1 AUTHOR
+
+  Brock Wilcox <awwaiid@thelackthereof.org>
+  http://thelackthereof.org/
+
+=head1 COPYRIGHT
+
+  Copyright (c) 2005 Brock Wilcox <awwaiid@thelackthereof.org>. All rights
+  reserved.  This program is free software; you can redistribute it and/or
+  modify it under the same terms as Perl itself.
+
+=cut
+
