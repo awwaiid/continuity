84aef04dd4b6bf3730b6c73c63c9c1eabe380b24
awwaiid <awwaiid@thelackthereof.org>
2006-03-04 03:00:40 -0500

    Make mapper use PID param instead of cookie to track sessions
    darcs-hash:20060304080040-4fc8e-14be78d2a2fd66e79269d27dd7060ce7b4a76395.gz

--- lib/Continuity/Mapper.pm
+++ lib/Continuity/Mapper.pm
@@ -50,22 +50,27 @@ my $sessionIdCounter;
 sub getSession {
   my ($self, $request) = @_;
   #print "Headers: " . $request->as_string();
-
-  my $cookieHeader = $request->header('Cookie');
-  $self->debug(3, "Cookies: $cookieHeader");
-  
-  $self->debug(3, "sessionIdCounter: $sessionIdCounter");
-  if($cookieHeader =~ /sessionid=(\d+)/) {
-    $self->debug(3, "Found sessionId!");
-    return $1;
+  my $pid = $request->params->{pid};
+  print STDERR "GOT PID: $pid\n";
+  if(defined $pid) {
+    return $pid;
   }
+
+  #my $cookieHeader = $request->header('Cookie');
+  #$self->debug(3, "Cookies: $cookieHeader");
+  
+  #$self->debug(3, "sessionIdCounter: $sessionIdCounter");
+  #if($cookieHeader =~ /sessionid=(\d+)/) {
+  #  $self->debug(3, "Found sessionId!");
+  #  return $1;
+  #}
   return $sessionIdCounter++;
 }
 
 sub map {
   my ($self, $request, $conn) = @_;
   my $c;
-  my $sessionId = $self->getSession($request);
+  my $sessionId = $self->getSession($request) + 0;
   if(defined $self->{continuations}{$sessionId}) {
     $c = $self->{continuations}{$sessionId};
   } else {
@@ -79,7 +84,11 @@ sub map {
 
   # And send our session cookie
   # Perhaps instead we should be adding this to a list of headers to be sent
-  print $conn "Set-Cookie: sessionid=$sessionId\r\n";
+  #print $conn "Set-Cookie: sessionid=$sessionId\r\n";
+  print STDERR "Setting client pid = $sessionId\n";
+  $request->uri->query(
+    $request->uri->query() . '&pid=' . $sessionId
+  );
   return $c;
 }
 
