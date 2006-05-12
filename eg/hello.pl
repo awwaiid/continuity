4e1feeabd3789f2ace71cfd6822ad24f67358049
awwaiid <awwaiid@thelackthereof.org>
2006-05-12 01:22:10 -0400

    update hello.pl to use new API
    darcs-hash:20060512052210-4fc8e-fd3aaf7c3758e8b575c2b653dd6b112d2cf7dcaa.gz

--- eg/hello.pl
+++ eg/hello.pl
@@ -1,16 +1,29 @@
 #!/usr/bin/perl
+use lib '../lib';
+use strict;
+use warnings;
+use Coro;
+use Coro::Event;
 
-use lib '..';
-use Continuity::Server::Simple;
-$server = new Continuity::Server::Simple;
-#$server->ignore_path('/siteicon.ico');
-$server->loop();
+use Continuity;
+my $server = new Continuity(
+  path_session => 1
+);
 
 sub main {
+  my $request = shift;
+STDERR->print(__FILE__, ' ', __LINE__, "\n");
+  $request->next;
+STDERR->print(__FILE__, ' ', __LINE__, "\n");
   # must do a substr to chop the leading '/'
-  $name = substr($server->get_request->url->path,1) || 'World';
-  print "Hello, $name!";
-  $name = substr($server->get_request->url->path,1) || 'World';
-  print "Hello to you too, $name!";
+  my $name = substr($request->{request}->url->path, 1) || 'World';
+  $request->print("Hello, $name!");
+STDERR->print(__FILE__, ' ', __LINE__, "\n");
+  $request->next;
+STDERR->print(__FILE__, ' ', __LINE__, "\n");
+  $name = substr($request->{request}->url->path, 1) || 'World';
+  $request->print("Hello to you too, $name!");
+STDERR->print(__FILE__, ' ', __LINE__, "\n");
 }
 
+Event::loop();
