297e1bbb9bf5fb1ca46b4939ff0125197309faa1
awwaiid <awwaiid@thelackthereof.org>
2006-10-20 02:49:54 -0400

    Add chat-ajax-push demo
    darcs-hash:20061020064954-4fc8e-5327ba15f489dec17b62ef5f6964bcbc44987a95.gz

---
+++ eg/chat-ajax-push.js
@@ -0,0 +1,69 @@
+
+var poll_count = 0;
+var sid = Math.random();
+
+function new_request() {
+  var req;
+  if (window.XMLHttpRequest) {
+    req = new XMLHttpRequest();
+  } else if (window.ActiveXObject) {
+    req = new ActiveXObject("Microsoft.XMLHTTP");
+  } 
+  return req;
+}
+
+function do_request(url, callback) {
+  var req = new_request();
+  if(req != undefined) {
+    req.onreadystatechange = function() {
+      if (req.readyState == 4) { // only if req is "loaded"
+        if (req.status == 200) { // only if "OK"
+          if(callback) callback(req.responseText);
+        } else {
+          alert("AJAX Error:\r\n" + req.statusText);
+        }
+      }
+    }
+    req.open("GET", url, true);
+    req.send("");
+  }
+}
+
+function setup_poll() {
+  setTimeout('poll_server()',100);
+}
+
+function poll_server() {
+  document.getElementById('status').innerHTML = 'Polling ('+(poll_count++)+')...';
+  do_request('http://localhost:16001/pushstream/' + sid + '/' + Math.random(), got_update);
+}
+
+function got_update(txt) {
+  document.getElementById('status').innerHTML = 'Got update.'
+  if(document.getElementById("log").innerHTML != txt)
+    document.getElementById("log").innerHTML = txt;
+  setup_poll();
+}
+
+function message_sent(result) {
+  document.getElementById("message").value = "";
+  document.getElementById("message").focus();
+}
+
+function send_message() {
+  var username = document.getElementById("username").value;
+  var message = document.getElementById("message").value;
+  do_request('http://localhost:16001/' + sid + '?x=' + Math.random() + '&username=' + username + '&message=' + message, message_sent);
+  return false;
+}
+
+function init() {
+  setup_poll();
+  //document.getElementById("sendbutton").onclick = send_message;
+  send_message();
+  document.getElementById("f").onsubmit = send_message;
+  //document.getElementById("username").focus();
+}
+
+window.onload = init;
+
---
+++ eg/chat-ajax-push.pl
@@ -0,0 +1,79 @@
+#!/usr/bin/perl
+
+use strict;
+use lib '../lib';
+use Continuity;
+
+my @messages;
+my $got_message;
+
+my $server = Continuity->new(
+  port => 16001,
+  path_session => 1,
+  staticp => sub { $_[0]->url->path =~ m/\.(jpg|gif|png|css|ico|js)$/ },   
+);
+
+$server->loop;
+
+sub pushstream {
+  my ($req) = @_;
+  while(1) {
+    print STDERR "**** Waiting for message ****\n";
+    my $w = Coro::Event->var(var => \$got_message, poll => 'w');
+    $w->next;
+    $w->cancel;
+    print STDERR "**** GOT MESSAGE, SENDING ****\n";
+    my $log = join "<br>", @messages;
+    $req->print($log);
+    $req->next;
+  }
+}
+
+sub main {
+  my ($req) = @_;
+  $req->next;
+
+  if($req->request->url->path =~ /pushstream/) {
+    pushstream($req);
+  }
+
+  print STDERR "Path: '" . $req->request->url->path . "'\n";
+
+  # We only send them the main HTML one time. Optimistic, eh?
+  if($req->request->url->path eq '/') {
+  while(1) {
+  $req->print(qq{
+    <html>
+      <head>
+        <title>Chat!</title>
+        <script src="chat-ajax-push.js" type="text/javascript">></script>
+      </head>
+      <body>
+        <form id=f>
+        <input type=text id=username name=usernamename size=10>
+        <input type=text id=message name=message size=50>
+        <input type=submit name="sendbutton" value="Send" id="sendbutton">
+        <span id=status></span>
+        </form>
+        <br>
+        <div id=log>-- no messages yet --</div>
+      </body>
+    </html>
+  });
+  $req->next;
+  }
+  }
+
+  while(1) {
+    $req->next;
+    $got_message = 1;
+    my $msg = $req->param('message');
+    my $name = $req->param('username');
+    if($msg) {
+      unshift @messages, "$name: $msg";
+      pop @messages if $#messages > 15;
+    }
+    $req->print("Got it!");
+  }
+}
+
