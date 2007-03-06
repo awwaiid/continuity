8caa7044744627eb22321becf597f7d1cc3370a8
awwaiid <awwaiid@thelackthereof.org>
2007-03-06 16:36:01 -0500

    Make post-redirect example show cached input values
    darcs-hash:20070306213601-4fc8e-b94f2b5f481ab57c350b44ee292c8a86b617ea56.gz

--- eg/post-redirect.pl
+++ eg/post-redirect.pl
@@ -1,5 +1,17 @@
 #!/usr/bin/perl
 
+# The idea here is to find a way to deal with the back button well. We will
+# send a 302 status between each page. Then we can process their data and give
+# them a new ID. When they hit 'back', it doesn't re-processes their data.
+#
+# Alternatively, maybe the 302 can be used as not just a back-button indicator,
+# but as an on-purpose 'block' to keep them from backing out of the site (going
+# back past a transaction, for example). The back button would still thus work,
+# but only _within_ the site, and we'd have server-side programatic control
+# over what happens when people use it.
+#
+# Or something like that.
+
 use strict;
 use lib '../lib';
 use Continuity;
@@ -14,6 +26,7 @@ sub main {
   my $msg = '';
   my %cache;
   my $count = 0;
+  my %num_memory;
   while(1) {
     my $next_pageID = sprintf "%x", int rand 0xffffffff;
     print STDERR "Displaying form. NextPID: $next_pageID\n";
@@ -33,9 +46,7 @@ sub main {
     $msg = '';
     $request->{no_content_type} = 1;
     $request->next;
-    $num = $request->param('num');
     $pageID = $request->param('pageID');
-    print STDERR "Num: $num\tPageID: $pageID\n";
     if($cache{$pageID}) {
       print STDERR "Already been here...\n";
       if($cache{$pageID} == $count - 1) {
@@ -43,9 +54,13 @@ sub main {
       } else {
         $msg = "BACK detected ($cache{$pageID})!";
       }
+      $num = $num_memory{$cache{$pageID}}; # Lets just get the num from before
     } else {
       $cache{$pageID} = $count++;
+      $num = $request->param('num');
+      $num_memory{$cache{$pageID}} = $num;
     }
+    print STDERR "Num: $num\tPageID: $pageID\n";
     print STDERR "Doing redirect after POST\n";
     $request->request->conn->send_redirect("/?pageID=$pageID",303);
     $request->{no_content_type} = 0;
