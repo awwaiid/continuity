9b989fe415ac1778ffedade610137de356266102
awwaiid <awwaiid@thelackthereof.org>
2007-03-31 11:57:21 -0400

    slight formatting fix for eg/chat.pl (simple chat)
    darcs-hash:20070331155721-4fc8e-32c9c6c8116068a1e936ebca1495dae088be7e4d.gz

--- eg/chat.pl
+++ eg/chat.pl
@@ -30,10 +30,9 @@ sub main {
         </head>
         <body>
           <form id=f>
-          <input type=text id=username name=username size=10 value="$username">
-          <input type=text id=message name=message size=50>
-          <input type=submit name="sendbutton" value="Send" id="sendbutton">
-          <span id=status></span>
+            <input type=text id=username name=username size=10 value="$username">
+            <input type=text id=message name=message size=50>
+            <input type=submit name="sendbutton" value="Send" id="sendbutton">
           </form>
           <br>
           <div id=log>$messages_html</div>
