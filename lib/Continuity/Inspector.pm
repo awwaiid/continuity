4e0a3031b0bb6d44f37b529d8b744ec885ad08d2
scott <scott@slowass.net>
2007-05-27 21:24:15 -0400

    actually *add* the Continity::Inspector
    darcs-hash:20070528012415-bb130-fc715844a61b271aa63355916e42a193a615e99d.gz

---
+++ lib/Continuity/Inspector.pm
@@ -0,0 +1,50 @@
+package Inspector;
+use Data::Dumper;
+use Coro::Event;
+
+sub new {
+  my $class = shift;
+  my %args = @_;
+  my $self = { 
+    peeks_pending => \my $peeks_pending, 
+    requester => $args{requester},
+    callback => $args{callback},
+  };
+  bless $self, $class;
+  return $self;
+}
+
+sub inspect {
+    my $self = shift;
+    my $queue = shift;
+    ${ $self->{peeks_pending} } = 1;
+    $queue->put($self);
+    my $var_watcher = Coro::Event->var( var => $self->{peeks_pending}, poll => 'w', );
+    while( ${ $self->{peeks_pending} } ) {
+print STDERR "spin\n";
+        $var_watcher->next;
+print STDERR "spun\n";
+    }
+    $var_watcher->stop;
+    $var_watcher->cancel;
+    return undef;
+}
+
+sub immediate {
+  my $self = shift;
+  my $requester = $self->{requester};
+  $self->{callback}->(requester => $requester); # XXX API?  pass $self and solidify?  or just pass a few vars?
+  ${ $self->{peeks_pending} } = 0;
+  return 1;
+}
+
+sub end_request { }
+
+sub send_basic_headers { }
+
+sub close { }
+
+sub send_error { }
+
+1;
+
