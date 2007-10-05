9f2d86186da1c65a7cc336a3541fef4023641f1b
awwaiid <awwaiid@thelackthereof.org>
2007-10-04 23:27:58 -0400

    Allow for invalid input in eg/guess.pl
    darcs-hash:20071005032758-4fc8e-d9532520b55bdcf06165ddecff0e1043566eb8a4.gz

--- eg/guess.pl
+++ eg/guess.pl
@@ -39,11 +39,13 @@ sub main {
             Hi! I'm thinking of a number from 1 to 100... can you guess it?<br>
     };
     do {
-      $tries++;
       $request->print($out);
       $guess = getNum($request);
-      $out .= "It is smaller than $guess.<br>\n" if $guess > $number;
-      $out .= "It is bigger than $guess.<br>\n" if $guess < $number;
+      if($guess) {
+        $tries++;
+        $out .= "It is smaller than $guess.<br>\n" if $guess > $number;
+        $out .= "It is bigger than $guess.<br>\n" if $guess < $number;
+      }
     } until ($guess == $number);
     $request->print("You got it! My number was in fact $number.<br>\n");
     $request->print("It took you $tries tries.<br>\n");
