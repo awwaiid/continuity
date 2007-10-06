25c496d4651ee3038fc93c42b9b9454d091bf479
awwaiid <awwaiid@thelackthereof.org>
2007-10-06 00:02:35 -0400

    Remove extra includes and debugging from eg/hello.pl
    darcs-hash:20071006040235-4fc8e-2aa02846735561bc47aec3af0687229f43796ce7.gz

--- eg/hello.pl
+++ eg/hello.pl
@@ -2,10 +2,8 @@
 use lib '../lib';
 use strict;
 use warnings;
-use Coro;
-use Coro::Event;
-
 use Continuity;
+
 my $server = new Continuity(
   path_session => 1,
   port => 8080
@@ -15,15 +13,14 @@ $server->loop;
 
 sub main {
   my $request = shift;
-STDERR->print(__FILE__, ' ', __LINE__, "\n");
+
   # must do a substr to chop the leading '/'
   my $name = substr($request->{request}->url->path, 1) || 'World';
+
   $request->print("Hello, $name!");
-STDERR->print(__FILE__, ' ', __LINE__, "\n");
   $request->next;
-STDERR->print(__FILE__, ' ', __LINE__, "\n");
+
   $name = substr($request->{request}->url->path, 1) || 'World';
   $request->print("Hello to you too, $name!");
-STDERR->print(__FILE__, ' ', __LINE__, "\n");
 }
 
