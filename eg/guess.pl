29dd4eae2891b096841e9c76514e410f543bf641
awwaiid <awwaiid@thelackthereof.org>
2007-10-06 18:24:37 -0400

    Simplify guess.pl a bit, and unify indenting
    darcs-hash:20071006222437-4fc8e-3ea3e2a236d22d2a996fe6e8a00a0335f96ffaa0.gz

--- eg/guess.pl
+++ eg/guess.pl
@@ -17,8 +17,6 @@ sub getNum {
     Enter Guess: <input name="num" id=num>
     <script>document.getElementById('num').focus()</script>
     </form>
-    </body>
-    </html>
   });
   $request = $request->next;
   my $num = $request->param('num');
@@ -27,28 +25,24 @@ sub getNum {
 
 sub main {
   my $request = shift;
-
-    my $guess;
-    my $number = int(rand(100)) + 1;
-    my $tries = 0;
-    my $out = qq{
-      <html>
-        <head><title>The Guessing Game</title></head>
-        <body>
-          <form method=POST>
-            Hi! I'm thinking of a number from 1 to 100... can you guess it?<br>
-    };
-    do {
-      $request->print($out);
-      $guess = getNum($request);
-      if($guess) {
-        $tries++;
-        $out .= "It is smaller than $guess.<br>\n" if $guess > $number;
-        $out .= "It is bigger than $guess.<br>\n" if $guess < $number;
-      }
-    } until ($guess == $number);
-    $request->print("You got it! My number was in fact $number.<br>\n");
-    $request->print("It took you $tries tries.<br>\n");
+  my $guess;
+  my $number = int(rand(100)) + 1;
+  my $tries = 0;
+  my $out = qq{
+    <form method=POST>
+      Hi! I'm thinking of a number from 1 to 100... can you guess it?<br>
+  };
+  do {
+    $request->print($out);
+    $guess = getNum($request);
+    if($guess) {
+      $tries++;
+      $out .= "It is smaller than $guess.<br>\n" if $guess > $number;
+      $out .= "It is bigger than $guess.<br>\n" if $guess < $number;
+    }
+  } until ($guess == $number);
+  $request->print("You got it! My number was in fact $number.<br>\n");
+  $request->print("It took you $tries tries.<br>\n");
 }
 
 1;
