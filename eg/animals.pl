f1f31fbc294a380dca3f47a19a76423368098f82
awwaiid <awwaiid@thelackthereof.org>
2008-03-11 23:57:27 -0400

    Update animal guessing game to work again
    darcs-hash:20080312035727-4fc8e-d32793767ff9f93e0f1e7dc9552df4839a481e2f.gz

--- eg/animals.pl
+++ eg/animals.pl
@@ -3,80 +3,76 @@
 use strict;
 use lib '../lib';
 use Continuity;
-use Coro;
-use Coro::Event;
 use Data::Dumper;
+no warnings;
 
 # This is the A MODIFIED VERSION written by awwaiid.
 # The original version was written by Merlyn,
 # http://www.perlmonks.org/?node_id=200391
 
-## I originally wrote this for a column,
-## but haven't gotten around to using it yet.
-## just think of an animal, and invoke it.
-## It's an example of a self-learning game.
-## When you choose not to continue, it'll dump out
-## the data structure of knowledge it has accumulated.
+# We cheat -- we don't have to pass $request around because all of the
+# subroutines are in the scope of main (and thus share the originally passed
+# $request variable).
 
 my $info = "dog";
 
-use Continuity;
-my $server = new Continuity;
-
-Event::loop();
+my $server = Continuity->new;
+$server->loop;
 
 sub main {
   my $request = shift;
-  # Ignore the first input, it just indicates that they are viewing the page
-  $request->next;
+
   {
-    try($request, $info);
+    try($info);
     redo if (yes("play again?"));
   }
-  print "<pre>Bye!\n";
-  print Dumper($info);
-}
+  printout("<pre>Bye! Here's my DB");
+  printout(Dumper($info));
 
-sub try {
-  my $request = $_[0];
-  my $this = $_[1];
-  if (ref $this) {
-    return try($request, $this->{yes($request,$this->{Question}) ? 'Yes' : 'No' });
+  sub printout {
+    $request->print(@_);
   }
-  if (yes($request,"Is it a $this")) {
-    $request->print("I got it!\n");
-    return 1;
-  };
-  print "no!?  What was it then? ";
-  chomp(my $new = stdin());
-  print "And a question that distinguishes a $this from a $new would be? ";
-  chomp(my $question = stdin());
-  my $yes = yes("And for a $new, the answer would be...");
-  $_[1] = {
-           Question => $question,
-           Yes => $yes ? $new : $this,
-           No => $yes ? $this : $new,
-          };
-  return 0;
-}
 
-sub yes {
-  print "@_ (yes/no)?";
-  stdin() =~ /^y/i;
-}
+  sub stdin {
+    printout qq{
+      <form method=POST>
+        <input id=in name=in type=text>
+        <script>document.getElementById('in').focus();</script>
+      </form>
+    };
+    $request->next;
+    my $in = $request->param('in');
+    return $in;
+  }
 
-sub stdin {
-  print qq{
-    <form method=POST>
-      <input id=in name=in type=text>
-      <script>document.getElementById('in').focus();</script>
-    </form>
-  };
-  my $params = $server->get_request->params;
-  my $in = $params->{in};
-  return $in;
-}
+  sub try {
+    my $this = $_[0];
+    if (ref $this) {
+      return try($this->{yes($this->{Question}) ? 'Yes' : 'No' });
+    }
+    if (yes("Is it a $this")) {
+      printout("I got it!\n");
+      return 1;
+    };
+    printout("no!?  What was it then? ");
+    chomp(my $new = stdin());
+    printout("And a question that distinguishes a $this from a $new would be? ");
+    chomp(my $question = stdin());
+    my $yes = yes("And for a $new, the answer would be...");
+    $_[0] = {
+             Question => $question,
+             Yes => $yes ? $new : $this,
+             No => $yes ? $this : $new,
+            };
+    return 0;
+  }
 
+  sub yes {
+    printout "@_ (yes/no)?";
+    stdin() =~ /^y/i;
+  }
+
+}
 
 1;
 
