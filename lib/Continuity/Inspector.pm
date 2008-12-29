3354b13f50a25dbd9f079f1482108a3235a3280a
scott <scott@slowass.net>
2008-12-29 12:37:17 -0500

    kill a race condition on watching for the inspector callback to finish and make server a required parameter to C::Monitor
    darcs-hash:20081229173717-bb130-31b1bc769a8f4990ed4d9f4b5164a1e96e6519a7.gz

--- lib/Continuity/Inspector.pm
+++ lib/Continuity/Inspector.pm
@@ -2,7 +2,9 @@ package Continuity::Inspector;
 
 use strict;
 use Data::Dumper;
-use Coro::Event;
+# use Coro::Event;
+use Coro;
+use Coro::Semaphore;
 
 # Accessors
 sub debug_level { exists $_[1] ? $_[0]->{debug_level} = $_[1] : $_[0]->{debug_level} }
@@ -11,9 +13,9 @@ sub debug_callback { exists $_[1] ? $_[0]->{debug_callback} = $_[1] : $_[0]->{de
 sub new {
   my $class = shift;
   my %args = @_;
-  my $self = { 
-    peeks_pending => \my $peeks_pending, 
-    requester => $args{requester},
+  my $self = {
+    peeks_pending => undef,
+    # requester => $args{requester}, # pointless
     callback => $args{callback},
     debug_level => $args{debug_level} || 1,
     debug_callback => $args{debug_callback} || sub { print "@_\n" },
@@ -25,26 +27,29 @@ sub new {
 sub inspect {
   my $self = shift;
   my $queue = shift;
-  ${ $self->{peeks_pending} } = 1;
+  $self->{peeks_pending} = Coro::Semaphore->new(0);
   $queue->put($self);
-  my $var_watcher = Coro::Event->var( var => $self->{peeks_pending}, poll => 'w', );
-  while( ${ $self->{peeks_pending} } ) {
-    $self->Continuity::debug(3, "spin");
-    $var_watcher->next;
-    $self->Continuity::debug(3, "spun");
-  }
-  $var_watcher->stop;
-  $var_watcher->cancel;
+  # sdw 200812 this deadlocks in a race condition where the variable gets diddled before we start watching it here
+  # my $var_watcher = Coro::Event->var( var => $self->{peeks_pending}, poll => 'w', );
+  # while( ${ $self->{peeks_pending} } ) {
+  #   $self->Continuity::debug(3, "spin");
+  #   $var_watcher->next;
+  #   $self->Continuity::debug(3, "spun");
+  # }
+  # $var_watcher->stop;
+  # $var_watcher->cancel;
+  $self->{peeks_pending}->down;
   return undef;
 }
 
 sub immediate {
   my $self = shift;
-  my $requester = $self->{requester};
-  $self->{callback}->(requester => $requester); # XXX API?  pass $self and solidify?  or just pass a few vars?
-  ${ $self->{peeks_pending} } = 0;
+  $self->{callback}->();
+  $self->{peeks_pending}->up; # ${ $self->{peeks_pending} } = 0;
   return 1;
 }
+
+# fake enough of the API that Continuity::RequestHolder doesn't blow up
 
 sub end_request { }
 
